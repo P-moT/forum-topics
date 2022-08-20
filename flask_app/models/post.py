edbaa2dae3ff142ecdc64215a9060bd149ac50da
P-moT <tommypeltier@gmail.com>
2022-08-20 02:49:46 -0400

    comments can be created and displayed on post.html

--- flask_app/models/post.py
+++ flask_app/models/post.py
@@ -1,5 +1,6 @@
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.user import User
 db = 'forums'
 
 class Post:
@@ -23,3 +24,30 @@ class Post:
         query = 'SELECT * FROM posts WHERE id = %(id)s;'
         results = connectToMySQL(db).query_db(query, data)
         return cls(results[0])
+
+
+class Comment:
+    def __init__(self, data):
+        self.id = data['id']
+        self.comment = data['comment']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.posts_id = data['posts_id']
+        self.users_id = data['users_id']
+        self.author = User(data)
+
+    @classmethod
+    def get_comments(cls, data):
+        query = 'SELECT * FROM comments JOIN users ON comments.users_id = users.id  WHERE posts_id = %(posts_id)s;'
+        results = connectToMySQL(db).query_db(query, data)
+        print(results)
+        comment_list = []
+        for each_comment in results:
+            comment_obj = cls(each_comment)
+            comment_list.append(comment_obj)
+        return comment_list
+
+    @classmethod
+    def create_comment(cls, data):
+        query = 'INSERT INTO comments (comment, users_id, posts_id) VALUES (%(comment)s, %(users_id)s, %(posts_id)s);'
+        return connectToMySQL(db).query_db(query, data)
